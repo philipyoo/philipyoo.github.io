1dd881ffd1c21df6142517b0c1bdf6075e15258c
Philip Yoo <philipyoo10@gmail.com>
2015-06-04 22:36:37 -0700

    Fix links, Update blog index

--- divload.js
+++ divload.js
@@ -1,11 +1,26 @@
-$(function () {
-    $("#nav_page_a").on("click", function () {
-        $("#wrapper").load("PageA.html");
-    });
-    $("#nav_page_b").on("click", function () {
-        $("#wrapper").load("PageB.html");
-    });
-});
+
+// Figure out this jquery click function for the miniblog posts
+
+
+$(document).ready(function(){
+  $("#nav_page_a").on("click", function () {
+    console.log( $(this).text('hi'));
+    // $("#wrapper").load("../miniblog/blog_one.html");
+  });
+  $("#nav_page_b").on("click", function () {
+    $("#wrapper").load("../miniblog/blog_two.html");
+  });
+
+})
+
+// $(function () {
+//     $("#nav_page_a").click(function () {
+//         $("#wrapper").load("../miniblog/blog_one.html");
+//     });
+//     $("#nav_page_b").on("click", function () {
+//         $("#wrapper").load("../miniblog/blog_two.html");
+//     });
+// });
 
 
 // http://api.jquery.com/load/
