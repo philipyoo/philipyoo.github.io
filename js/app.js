817703f13aae17d06a8e1ea93db6fb9da72d3632
Philip Yoo <philipyoo10@gmail.com>
2016-01-04 22:06:46 -0800

    Clean up unnecessary code in js and html file

--- js/app.js
+++ js/app.js
@@ -1,77 +1,5 @@
 // $(document).foundation();
 
-// TODO: Use json information in `philipyoo.json`
-var testThis;
-
-$.getJSON("./philipyoo.json", function(info) {
-  console.log(info);
-  testThis = info;
-})
-
-console.log('testing: ');
-console.log(testThis);
-
-var philipyoo = {
-  "name" : "Philip Yoo",
-  "location" : "San Diego",
-  "gender" : "male",
-  "about" : "Hello and thank you for dropping by! My name is Philip Yoo and I spent most of my life in San Diego, CA. I attended and graduated from Dev Bootcamp in San Francisco. I am an aspiring full-stack developer who is currently looking for opportunities to apply what I have learned so far and continue learning in order to be the best that I could be as a developer.",
-  "random" : [
-    "I could live off of Burritos! (Seriously)",
-    "Huge fan of Milk Tea with Boba.",
-    "I enjoy playing Texas Hold'em Poker NL >:)",
-    "First programming language: Ruby.",
-    "Guilty Pleasure: Korean music and sports-related cartoons",
-    "I'm a Semi-Foodie?",
-    "Coined the term 'Philip Yoo is Awesome'"
-  ],
-  "resume" : {
-    "header" : ["Philip Yoo", "Full-Stack Developer"],
-    "skills" : [
-      "Programming Languages",
-      ["Ruby", "JavaScript"],
-      "Databases",
-      ["SQLite", "PostgreSQL", "Mongo"],
-      "Web Frameworks, Libraries & Others",
-      ["Ruby on Rails", "NodeJS", "Sinatra", "AJAX", "jQuery", "React", "ActiveRecord", "HTML", "CSS"]
-    ],
-    "summary" : [
-      "After graduating college, I tried my hand at a lot of different jobs. I interacted with a wide range of people and gained a lot of invaluable experiences, and yet there was still something missing-- everything was still \"just a job\". Reflecting on my past experiences, I knew I wanted to build things that would benefit others. In pursuit of something more, I dropped everything and wholly dedicated myself to learning to program.",
-      "I attended Dev Bootcamp, where I spent 1000+ hours learning to program applications and build websites. I worked on projects in pairs or in groups and was able to clearly communicate ideas to the team. I learned about engineering empathy and how that can affect a team's performance. I approached many of my projects structurally from a user's point-of-view while applying Agile development. I picked up new technologies within days and applied them to relevant projects. And...I did this all while enjoying the experience.",
-      "I am a full-stack web developer who has a strong desire to build meaningful and fun applications. I am currently seeking opportunities to apply and expand upon my current skill sets. I am a quick learner who enjoys problem-solving, gaming, and contributing to a team environment."
-    ],
-    "education" : [
-      "Dev Bootcamp", ["Web Development (2015)"],
-      "University of San Diego", ["Bachelor of Business Administration (2010)"]
-    ],
-    "employment" : [
-      "City National Bank, Relationship Banker", ["Carlsbad, CA (Feb.14 - Jan.15)"],
-      "Accend Energy, Energy Efficiency Consultant", ["San Diego, CA (Dec.12 - Dec.13)"],
-      "Aventis Asset Management, Research Analyst Intern", ["Costa Mesa, CA (Jun.12 - Aug.12)"],
-      "USA Trading, Operations and Market Research", ["Chicago, IL (Jan.11 - May.12)"]
-    ]
-  },
-  "projects" : {
-    "The Game" : {
-      "info" : "'The Game' is a 2D Multiplayer Sidescrolling Shooting game built using MongoDB, NodeJS, ExpressJS, Socket.IO, and Phaser.<br/> Built with a team of 5 over a 6 day period, our goal for this project was to discover making a shooting game that allows multiple players to play together. Our team implemented Agile development methodologies with Object-Orientation to help with the translation of data being passed to the sockets and attempted to use MVC design structure.<br/> My specific role in this project was front-end using Phaser, a JavaScript Physics Game Engine.",
-      "url" : "https://github.com/cpbasham/TheGame-0.2"
-    },
-    "Terminal Chess" : {
-      "info" : "A 2-player chess game playable via the terminal. Built with Ruby, I decided to start this project to practice building out a program that requires a lot of communication between different classes, organizing code to implement common Ruby structures like inheritance, and making the view portion fun and challenging by including user keystrokes as input for the program and using the Ruby colorize gem to add color and visuals to the game.",
-      "url" : "https://github.com/philipyoo/terminal-chess"
-    }
-  },
-  "socialMedia" : {
-    "blogger" : "http://philipyoo.blogspot.com/",
-    "email" : "mailto:philipyoo10@gmail.com",
-    "github" : "https://github.com/philipyoo",
-    "linkedin" : "https://www.linkedin.com/in/philipyoo",
-    "twitter" : "https://twitter.com/philipYoo10",
-    "stackoverflow" : "http://stackoverflow.com/users/4803039/philip-yoo"
-  }
-};
-
-
 
 var sidebarDiv = document.getElementById("social-media");
 var smKeys = Object.keys(philipyoo.socialMedia);
@@ -95,11 +23,6 @@ for (var i = 0; i < Object.keys(philipyoo.socialMedia).length; i++) {
   // Add the element to parent div `sidebarDiv`
   sidebarDiv.appendChild(element);
 }
-
-
-
-
-
 
 
 
